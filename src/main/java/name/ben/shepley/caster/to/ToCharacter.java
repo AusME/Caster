8c0777013121119c6814b3ea78ba764e8e707205
ausme <ausme2@gmail.com>
2020-03-19 17:25:50 -0600

    Fixed ToCharacter

--- src/main/java/name/ben/shepley/caster/to/ToCharacter.java
+++ src/main/java/name/ben/shepley/caster/to/ToCharacter.java
@@ -7,7 +7,7 @@ import name.ben.shepley.caster.configuration.RoundingModeEnum;
 
 import java.util.Optional;
 
-public class ToCharacter extends AbstractTo<ToCharacter> {
+public final class ToCharacter extends AbstractTo<ToCharacter> {
     public ToCharacter(CasterConfiguration casterConfiguration) {
         super(casterConfiguration);
 
@@ -48,7 +48,7 @@ public class ToCharacter extends AbstractTo<ToCharacter> {
     }
 
     public Optional<Character> cast(Number number) {
-        if (number.longValue() >= Character.MAX_VALUE) {
+        if (number.longValue() > Character.MAX_VALUE) {
             if (super.roundingModeEnum.equals(RoundingModeEnum.ONLY_EQUAL)) {
                 if (super.exceptionModeEnum.equals(ExceptionModeEnum.THROW)) {
                     throw new ArithmeticException("Attempted to cast a Number into a Character where the Number is larger than the Character's ability to store. ");
@@ -57,6 +57,16 @@ public class ToCharacter extends AbstractTo<ToCharacter> {
                 }
             } else {
                 return Optional.of(Character.MAX_VALUE);
+            }
+        } else if (number.longValue() < Character.MIN_VALUE) {
+            if (super.roundingModeEnum.equals(RoundingModeEnum.ONLY_EQUAL)) {
+                if (super.exceptionModeEnum.equals(ExceptionModeEnum.THROW)) {
+                    throw new ArithmeticException("Attempted to cast a Number into a Character where the Number is smaller than the Character's ability to store. ");
+                } else {
+                    return Optional.empty();
+                }
+            } else {
+                return Optional.of(Character.MIN_VALUE);
             }
         } else {
             return Optional.of((char) number.longValue());
